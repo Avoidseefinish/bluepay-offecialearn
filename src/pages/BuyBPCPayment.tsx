fa296d8951cb930933d268c5a50b1df68a281b05
Avoidseefinish <emmanuelstone1240@gmail.com>
2025-08-06 06:01:04 +0100

    Update BuyBPCPayment.tsx

--- src/pages/BuyBPCPayment.tsx
+++ src/pages/BuyBPCPayment.tsx
@@ -116,12 +116,12 @@ const BuyBPCPayment = () => {
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Account Number</p>
           <div className="flex justify-between items-center">
-            <p className="text-lg font-bold">5085319628</p>
+            <p className="text-lg font-bold">8103561349</p>
             <Button
               variant="default"
               size="sm"
               className="bg-blue-600 hover:bg-blue-700 flex items-center gap-1"
-              onClick={() => handleCopy("5085319628", "Account Number")}
+              onClick={() => handleCopy("8103561349", "Account Number")}
             >
               <Copy size={14} />
               Copy
@@ -136,7 +136,7 @@ const BuyBPCPayment = () => {
 
         <div className="mb-3 border-t pt-3">
           <p className="text-gray-500 text-xs">Account Name</p>
-          <p className="text-lg font-bold">TOLUWANI DAVID JOHNSON</p>
+          <p className="text-lg font-bold">ABDULLAHI SULEIMAN</p>
         </div>
       </div>
 
